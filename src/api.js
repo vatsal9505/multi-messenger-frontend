5e4f058766c50e8eaa529253cc104ffff554ad0d
Vatsal Kurhade <stonedelmo09@gmail.com>
2026-04-23 10:31:04 +0530

    fixed api file fully

--- src/api.js
+++ src/api.js
@@ -1,3 +1,17 @@
+const API_BASE = "https://multi-messenger-backend-production.up.railway.app";
+
+export const loginUser = async (payload) => {
+  const response = await fetch(`${API_BASE}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(payload)
+  });
+
+  return await response.json();
+};
+
 export const sendMessageApi = async (payload) => {
   const response = await fetch(`${API_BASE}/sendMessage`, {
     method: "POST",
